84fe4d863f8bb041a99229e688119a67108fb249
Epicawsomesauce <mtmarh@schools.vic.edu.au>
2025-07-19 12:04:55 +1000

    started menu test

---
+++ panorama test.py
@@ -0,0 +1,24 @@
+import cv2
+import glob
+import matplotlib.pyplot as plt
+import math
+
+#Read images
+
+imagefiles = glob.glob("IDK/*")
+imagefiles.sort()
+
+images = []
+for filename in imagefiles:
+    img = cv2.imread(filename)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    images.append(img)
+
+num_images = len(images)
+
+# Stitch Images
+Stitcher = cv2.Stitcher_create()
+status, result = Stitcher.stitch(images)
+if status == 0:
+    plt.figure(figsize=[30,10])
+    plt.imshow(result)
