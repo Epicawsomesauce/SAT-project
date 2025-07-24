61277dcdc9f3a74cb3ff625fa91d9954825ef33c
Epicawsomesauce <mtmarh@schools.vic.edu.au>
2025-07-25 09:34:55 +1000

    Added a timer to close the program while I try to figure out user inputs

--- Menu test.py
+++ Menu test.py
@@ -1,15 +1,27 @@
 import pygame, sys  # pygame is the foundation the game runs on and sys is what reads user inputs apparently
-
+from pygame.locals import *
 def main():
     pygame.init() # Must be used to allow pygame to be used
     SCREEN = pygame.display.set_mode((640, 480))
 
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
 
         SCREEN.fill((0,225,225))
         pygame.display.flip()
+        icon = pygame.image.load("basebsll.jpg")
+        pygame.display.set_icon(icon)
+        pygame.display.set_caption("It's a work in progress")
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            pygame.display.update
+            if KeyboardInterrupt:
+                    pygame.time.set_timer(pygame.QUIT, 3000)
+   
+
+
+
+    
 
 main()
