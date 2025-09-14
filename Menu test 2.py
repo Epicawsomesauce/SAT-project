58abcc9feaec2a90071459014013403dc05747fd
Epicawsomesauce <mtmarh@schools.vic.edu.au>
2025-09-14 10:05:51 +1000

    more menu stuff

--- Menu test 2.py
+++ Menu test 2.py
@@ -49,10 +49,43 @@ class Button():
 			self.text = self.font.render(self.text_input, True, self.base_color)
 
 def play():
-      print("AwesomeSauce")
+    while True:
+          
+        PLAY_MOUSE_POS = pygame.mouse.get_pos()
+    
+        SCREEN.fill((0,225,225))
+    
+        PLAY_TEXT = get_font(45).render("GAME GOES HERE.", True, "White")
+        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 260))
+        SCREEN.blit(PLAY_TEXT, PLAY_RECT)
+
+        PLAY_BACK = Button(image=None, pos=(640, 460), 
+            text_input="BACK", font=get_font(75), base_color="White", hovering_color="Green")
+
+        PLAY_BACK.changeColor(PLAY_MOUSE_POS)
+        PLAY_BACK.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                 if event.key == K_ESCAPE:
+                      pygame.quit()
+                      sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
+                    main()
+        pygame.display.update()
+ 
+        
+
+
+
+
+
 def quit():
       pygame.quit()
       sys.exit()
+
+
 def duck():
       mixer.music.play()
       pygame.time.wait(500)
@@ -70,10 +103,10 @@ def main():
         MENU_MOUSE_POS = pygame.mouse.get_pos()
         PLAY_BUTTON = Button(image=pygame.image.load("basebsll.png"), pos=(540, 250), 
                             text_input="AWESOME SAUCE", font=get_font(75), base_color="#ff9ae2", hovering_color="#ff86dd")
-        QUIT_BUTTON= Button(image=pygame.image.load("Peashooter.png"), pos=(810, 400), 
+        QUIT_BUTTON= Button(image=pygame.image.load("Peashooter.png"), pos=(760, 400), 
                             text_input="LEAVE", font=get_font(75), base_color="#ff9ae2", hovering_color="#ff86dd")
-        DUCK_BUTTON= Button(image=pygame.image.load("duck.png"), pos=(270, 400), 
-                            text_input="LEAVE", font=get_font(75), base_color="#ff9ae2", hovering_color="#ff86dd")
+        DUCK_BUTTON= Button(image=pygame.image.load("duck.png"), pos=(330, 400), 
+                            text_input="DUCK", font=get_font(75), base_color="#ff9ae2", hovering_color="#ff86dd")
 
         for button in [PLAY_BUTTON, QUIT_BUTTON, DUCK_BUTTON]:
             button.changeColor(MENU_MOUSE_POS)
